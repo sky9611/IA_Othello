54e49de72c2de1527e08cb6f8e57574c630713e1
sky9611 <collinbeyond@gmail.com>
2017-10-24 21:47:48 +0200

    ai4 finished(To be tested)

--- ai4.pl
+++ ai4.pl
@@ -13,7 +13,7 @@ chooseMove4(AI,X,Y,Board):-
     .
 
 minimax(AI, Board, [X,Y], Depth) :-
-    writeln('Enter minimax/4'),
+    %writeln('Enter minimax/4'),
     minimax(AI,Depth, Board, AI, _, [X,Y]).
 
 /* minimax(+Depth, +Position, +Player, -BestValue, -BestMove) :-
@@ -22,13 +22,16 @@ minimax(AI, Board, [X,Y], Depth) :-
       Player indicates if this move is by player (1) or opponent (-1).
 */
 
-minimax(AI, 0, Board, _, Eval, Move) :-
-      writeln(Move),
-      writeln('Enter end of minimax/6'),
-      ai3:eval(AI,Board,Eval).
+minimax(AI, 0, Board, _, Eval, _) :-
+      %writeln('Enter end of minimax/6'),
+      ai3:eval(AI,Board,Eval)
+      %displayBoard(Board),
+      %writeln(Eval),
+      %writeln('')
+      .
 
 minimax(AI,D, Board, CurrentPlayer, Eval, Move) :-
-      writeln('Enter minimax/6'),
+      %writeln('Enter minimax/6'),
       D > 0,
       D1 is D - 1,
       findall([X,Y],getLegalMove(CurrentPlayer,X,Y,Board),MoveList),
@@ -40,16 +43,19 @@ minimax(AI,D, Board, CurrentPlayer, Eval, Move) :-
       Player indicates if we are currently minimizing (-1) or maximizing (1).
       Move0 records the best move found so far and Value0 its value.
 */
-findBestMove(_,[], _, _, _, Eval, BestMove, Eval, BestMove):-
-      writeln(BestMove),
-      writeln('Enter end of findBestMove').
+findBestMove(_,[], _, _, _, Eval, BestMove, Eval, BestMove).
+      %writeln(BestMove),
+      %writeln('Enter end of findBestMove').
 
 findBestMove(AI,[[X,Y]|Moves],Board,D,CurrentPlayer,Eval0,Move0,BestValue,BestMove):-
-      writeln([[X,Y]|Moves]),
-      writeln('Enter findBestMove'),
+      %writeln('Enter findBestMove'),
+      %writeln([[X,Y]|Moves]),
+      %write('Eval0: '),writeln(Eval0),
       fillAndFlip(X,Y,CurrentPlayer,Board,NewBoard),
       Oppo is -CurrentPlayer,
       minimax(AI, D, NewBoard, Oppo, Eval, _OppoMove),
-      ( (AI =:= CurrentPlayer,Eval > Eval0) -> findBestMove(AI,Moves,Board,D,CurrentPlayer, Eval, [X,Y],BestValue,BestMove);
-      (AI =:= -CurrentPlayer,Eval < Eval0) -> findBestMove(AI,Moves,Board,D,CurrentPlayer, Eval, [X,Y],BestValue,BestMove);
+      %write('Eval: '),writeln(Eval),
+      ( (AI =:= CurrentPlayer,Eval >= Eval0) -> findBestMove(AI,Moves,Board,D,CurrentPlayer, Eval, [X,Y],BestValue,BestMove);
+      (AI =:= -CurrentPlayer,Eval =< Eval0) -> findBestMove(AI,Moves,Board,D,CurrentPlayer, Eval, [X,Y],BestValue,BestMove);
+      %writeln('asdasdasd'),
       findBestMove(AI,Moves,Board,D,CurrentPlayer,Eval0,Move0,BestValue,BestMove)).
