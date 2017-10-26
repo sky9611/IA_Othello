737e404bbffda166810cc3f53b6954844ffcbc31
echire <33063513+echire@users.noreply.github.com>
2017-10-26 12:28:16 +0200

    Update pve.pl
    Commentaires

--- pve.pl
+++ pve.pl
@@ -1,26 +1,36 @@
 :-module('pve',[play/0]).
 :-use_module([library(lists),io,fill,end,utils,ai1,ai3,ai4,ai2,ai5]).
 
+%Initialise le jeu
+
 play :-
     io:welcome,
     utils:initialBoard( Board ),
     io:displayBoard( Board ),
-    chooseColor(_,AI),
-    chooseLevel(D),
+    chooseColor(_,AI),          %Choisi la couleur
+    chooseLevel(D),             %Choisi le niveau de l'ia, puis lance la boucle correspondant à l'ia choisie
     (   D=:=1->play1(-1,AI,Board);
     D=:=2->play2(-1,AI,Board);
     D=:=3->play3(-1,AI,Board);
     D=:=4->play4(-1,AI,Board);
     D=:=5->play5(-1,AI,Board)).
 
+%Permet au joueur de choisir la couleur avec laquelle il va jouer
 chooseColor(Player,AI):-
     writeln('play white or black?(1 for white, -1 for black)'),
     read(Player),
     utils:changePlayer(Player,AI).
-
+    
+%Permet au joueur de choisir l'ia
 chooseLevel(D):-
     writeln('play with which level of AI?(...)'),
     read(D).
+
+/* Chaque boucle correspond à une ia.
+   La structure est similaire au play de PVP.
+   La seule différence est l'apparition d'un if afin de différencier si c'est le tour de l'humain ou de l'ia.
+   Si c'est à l'humain de jouer, le principe est le même que dans PVP.
+   Si c'est à l'ia de jouer, on appelle le chooseMove correspondant à l'ia afin de déterminer le coup, puis le board est mis à jour, affiché et on change de joueur.
 
 play1(Player,AI,Board):-
     (   end:noMoreLegalSquares(Board)->end:printScore(Board),end:winner(Board,_);
