e12bfad80a127ae740603aa07aeb545a9856d701
echire <33063513+echire@users.noreply.github.com>
2017-10-26 10:58:56 +0200

    Update ai2.pl
    Commentaires

--- ai2.pl
+++ ai2.pl
@@ -4,22 +4,25 @@
 
 
 chooseMove2(AI,X,Y,Board):-
-    findall([X,Y],getLegalMove(AI,X,Y,Board),MoveList),
-    maplist(ai3:fillAndFlipTemp(Board,AI),MoveList,BoardList),
-    maplist(evalSimple(AI),BoardList,EvalList),
-    listMax(EvalList,Max),
-    getLegalMove(AI,X,Y,Board),
-    fillAndFlip(X,Y,AI,Board,NewBoard),
+    findall([X,Y],getLegalMove(AI,X,Y,Board),MoveList),             %Cherche tous les coups possibles de l'ia, les stocke dans MoveList
+    maplist(ai3:fillAndFlipTemp(Board,AI),MoveList,BoardList),      %Joue tous les coups possibles déterminés à l'étape précédente,stocke le résultat dans BoardList
+    maplist(evalSimple(AI),BoardList,EvalList),                     %Evalue la situation sur les nouveaux boards, stocke les résultats dans EvalList
+    listMax(EvalList,Max),                                          %Determine la situation la plus favorable
+    %Teste les X et Y possibles, choisi le couple qui permet d'avoir la valeur maximum de evalSimple
+    getLegalMove(AI,X,Y,Board),                                     
+    fillAndFlip(X,Y,AI,Board,NewBoard),                             
     evalSimple(AI,NewBoard,Max),
+    %Affiche le coup joué
     utils:retransformeX(N,X),
     utils:retransformeY(Al,Y),
     reportMove(AI,N,Al),
     !.
 
+%Appelle countPlayer en changeant l'ordre des variables pour le maplist
 evalSimple(Player,Board,E):-
     countPlayer(Board,Player,E).
 
-
+%Compte les pièces d'un joueur
 countPlayer(Board,Player,N):-
     nth0(0,Board,Line1),countPlayerLine(Line1,Player,N1),
     nth0(1,Board,Line2),countPlayerLine(Line2,Player,N2),
